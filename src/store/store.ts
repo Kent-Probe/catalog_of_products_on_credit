16b687d8775f04417133aa0501b010086c19d158
Kent-Probe <kevinshe01@gmail.com>
2024-12-28 16:45:52 -0500

    fist version finish

---
+++ src/store/store.ts
@@ -0,0 +1,28 @@
+import seed from "@/seed/seed.json";
+import { atom } from "nanostores";
+
+const productsValues = seed.products;
+
+export const products = atom(productsValues);
+export const productsWithFilters = atom(productsValues);
+
+export const filters = atom({
+  search: "",
+  financials: ["Celya", "Krediya", "Alo Cred"],
+  ram: {
+    min: 2,
+    max: 512,
+  },
+  rom: {
+    min: 8,
+    max: 1024,
+  },
+  camera: {
+    min: 5,
+    max: 299,
+  },
+  battery: {
+    min: 2000,
+    max: 10000,
+  },
+});
