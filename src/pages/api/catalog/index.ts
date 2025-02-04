ecfc54c8bc38914c4dcfd3a1f859538a02b9299d
Kent-Probe <kevinshe01@gmail.com>
2025-02-04 15:21:46 -0500

    fix add data in number

--- src/pages/api/catalog/index.ts
+++ src/pages/api/catalog/index.ts
@@ -27,54 +27,58 @@ type typeFormData = {
 };
 
 export const POST: APIRoute = async ({ request, redirect }) => {
-  console.log("--------------------------");
-  const formData = await request.formData();
-  const values = Object.fromEntries(formData.entries()) as typeFormData;
-
-  if (JSON.stringify(values) === "{}") {
-    return new Response("No se proporciono datos", { status: 400 });
-  }
-
-  const installments = values.quotas.split(",").map((item) => {
-    const values = item.split("-");
-    return {
-      quantity: Number(values[0]),
-      price: Number(values[1]),
-    };
-  });
-
-  const file = values.celImg;
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-
-  const webp = await sharp(buffer).webp().toBuffer();
-  const outputPath = path.join(process.cwd(), "src", "images", "catalog", `${values.brand}-${values.model}.webp`);
-  await fs.writeFile(outputPath, webp);
-
-  const valuesToSend = {
-    brand: values.brand,
-    model: values.model,
-    rear_camera_mp: Number(values.camera),
-    front_camera_mp: Number(values.cameraFront),
-    rom_memory_gb: Number(values.disk),
-    ram_memory_gb: Number(values.memory),
-    processor: values.processor,
-    screen_inches: Number(values.inches),
-    battery_mah: Number(values.battery),
-    financials: values.financials.split(", "),
-    savings: Number(values.price),
-    installments: installments,
-    image: `/src/images/catalog/${values.brand}-${values.model}.webp`,
-    isVisible: true,
-  };
-
   try {
+    const formData = await request.formData();
+
+    const values = Object.fromEntries(formData.entries()) as typeFormData;
+
+    console.log(values);
+    console.log(formData);
+
+    if (JSON.stringify(values) === "{}") {
+      return new Response("No se proporciono datos", { status: 400, statusText: "No se proporciono datos" });
+    }
+
+    const installments = values.quotas.split(",").map((item) => {
+      const values = item.split("-");
+      return {
+        quantity: Number(values[0]),
+        price: Number(values[1].replace(/\./g, "")),
+      };
+    });
+
+    const file = values.celImg;
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const webp = await sharp(buffer).webp().toBuffer();
+    const outputPath = path.join(process.cwd(), "src", "images", "catalog", `${values.brand}-${values.model}.webp`);
+    await fs.writeFile(outputPath, webp);
+
+    const valuesToSend = {
+      brand: values.brand,
+      model: values.model,
+      rear_camera_mp: values.camera,
+      front_camera_mp: values.cameraFront,
+      rom_memory_gb: values.disk,
+      ram_memory_gb: values.memory,
+      processor: values.processor,
+      screen_inches: values.inches,
+      battery_mah: values.battery,
+      financials: values.financials.split(", "),
+      savings: parseInt(values.price.replace(/\./g, "")),
+      installments: installments,
+      initial: parseInt(values.initial.replace(/\./g, "")),
+      image: `/src/images/catalog/${values.brand}-${values.model}.webp`,
+      isVisible: true,
+    };
+
     const db = getFirestore(app);
     const docRef = await db.collection("catalog");
     await docRef.add(valuesToSend);
   } catch (e) {
     await fs.unlink(outputPath);
-    return new Response("Error durante la creación", { status: 500 });
+    return new Response("Error durante la creación", { status: 400, statusText: "Error durante la creación" });
   }
   return redirect("/");
 };
